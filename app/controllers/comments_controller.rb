e23950ed2efa9adde2cd83b1019ab7209c090245
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-01 11:18:36 +0900

    空のコメントエラーに対する修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.new(comment_params)
-    if comment.save
-      redirect_to "/prototypes/#{comment.prototype.id}"
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to "/prototypes/#{@comment.prototype.id}"
     else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
